a0aaeaa11d72a1bf0a5ebb8d4df5d455e75baaaf
Andree Monette <andreecmonette@gmail.com>
2014-03-20 23:17:34 -0400

    Delqueens now importable as a module, N-queens now based on length of name (defaults to 8)

--- delqueens.py
+++ delqueens.py
@@ -1,25 +1,24 @@
-
 class delBoard:
-  def __init__(self):   
-    #self.boardState = boardState
-    #self.myPos = myPos
-    pass 
   def __del__(self):
+    global __builtins__
     if not hasattr(self, 'myPos'):
+      if not __name__ == "__main__":
+        __builtins__ = delBoard
+      __builtins__.nqueens = len(__name__) 
       __builtins__.ans = 0
       __builtins__.collected = 0
-      for j in xrange(8):
+      for j in xrange(__builtins__.nqueens):
         a = delBoard()
         a.boardState = ((set(),set(),set()))
         a.myPos = j
         a = 0
       print str(__builtins__.collected) + " board states traversed."
-      print "The number of 8-queens solutions is: " + str(__builtins__.ans)
+      print "The number of "+str(__builtins__.nqueens)+"-queens solutions is: " + str(__builtins__.ans)
       return
     __builtins__.collected += 1 
     if self.myPos in self.boardState[0] or self.myPos in self.boardState[1] or self.myPos in self.boardState[2]:
       return
-    if len(self.boardState[0]) == 7:
+    if len(self.boardState[0]) == __builtins__.nqueens - 1:
       __builtins__.ans += 1 
       return
     c = self.boardState[0].copy()
@@ -29,12 +28,13 @@ class delBoard:
     l.discard(-1)
     r = set(map(lambda x: x+1, self.boardState[2])).copy()
     r.add(self.myPos+1)
-    r.discard(8)
-    for k in xrange(8):
+    r.discard(__builtins__.nqueens)
+    for k in xrange(__builtins__.nqueens):
       b = delBoard()
       b.boardState = (c,l,r)
       b.myPos = k
 q = delBoard()
-print "Interrupt me!"
-while True:
-  pass
+if __name__ == "__main__":
+  print "Interrupt me!"
+  while True:
+    pass
---
+++ importqueens.py
@@ -0,0 +1,8 @@
+import delqueens as dell
+dell.__name__ = "hello"
+
+
+
+
+while True:
+  pass
